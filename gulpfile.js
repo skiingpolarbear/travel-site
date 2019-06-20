fa747d760859db055c4b5114f10715831c2e841e
owen <owenmperks@gmail.com>
2019-06-20 12:25:01 -0400

    update on files 6/20

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,7 @@
-var gulp = require('gulp'),
-watch = require('gulp-watch'),
-postcss = require('gulp-postcss'),
-autoprefixer = require('autoprefixer'),
-cssvars = require('postcss-simple-vars'),
-nested = require('postcss-nested');
+require('./gulp/tasks/styles');
+require('./gulp/tasks/watch');
 
-gulp.task('default', function() {
-	console.log("Hooray - you created a gulp task.");
-});
 
-gulp.task('html', function() {
-	console.log("imagine something useful here");
-});
 
-gulp.task('styles', function() {
-	return gulp.src('./app/assets/styles/styles.css')
-	.pipe(postcss([cssvars, nested, autoprefixer]))
-	.pipe(gulp.dest('./app/temp/styles'));
-});
 
-gulp.task('watch', function() {
 
-	watch('./app/index.html', function() {
-		gulp.start('html');
-	});
-
-	watch('./app/assets/styles/**/*.css', function() {
-		gulp.start('styles');
-	})
-});
